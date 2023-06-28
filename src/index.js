dc41345d314447888e06f9de8513dde1f7eab2ff
jdolkowski <jdolkowski@soldevelo.com>
2023-06-28 19:40:35 +0200

    CM-214: add translations

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ const DEFAULT_CONFIG = {
   "translations": [{ key: "en", messages: messages_en }],
 }
 
-export const TemplateModule = (cfg) => {
+export const LanguageCoreMISEnglish = (cfg) => {
   return { ...DEFAULT_CONFIG, ...cfg };
 }
